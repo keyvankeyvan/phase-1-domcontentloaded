7a3dc14a2676677981e9a1cf8915f7a71d74c0a6
Keyvan Alonso <keyvanalonso@gmail.com>
2022-04-07 17:24:51 -0400

    commit tree 2c48eb163b93963768070d23642807e8ec3c859e

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', () => {
+    const coolText = document.getElementById('text')
+    coolText.textContent = 'This is really cool!'
+})
